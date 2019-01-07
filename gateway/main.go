40f27d00e4a863c3c74d4e4c572111b5d21c3cb3
MahNassar <MHNassar1993>
2019-01-07 15:59:10 +0200

    Add PATCH method to allow methods

--- gateway/main.go
+++ gateway/main.go
@@ -29,6 +29,6 @@ func main() {
 
 func enableCORS(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE , PATCH")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
