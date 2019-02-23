4ab17edcbd79b5bdc1ddcf07a4af07850694a88d
MahNassar <MHNassar1993>
2019-02-23 20:48:23 +0200

    Add nginx proxy

--- gateway/main.go
+++ gateway/main.go
@@ -6,10 +6,10 @@ import (
 	initApp "api_gateway/gateway/core/init"
 	requestHandler "api_gateway/gateway/core/request"
 	monitors "api_gateway/gateway/core/monitor"
-	"golang.org/x/crypto/acme/autocert"
+	//"golang.org/x/crypto/acme/autocert"
 	"time"
-	"context"
-	"crypto/tls"
+	//"context"
+	//"crypto/tls"
 )
 
 func main() {
@@ -26,33 +26,33 @@ func main() {
 		requestHandler.HttpHandler(w, r, Router)
 	})
 
-	go func() {
-		var m *autocert.Manager
-		hostPolicy := func(ctx context.Context, host string) error {
-			// Note: change to your real host
-			allowedHost := "sandbox.bolt-play.com"
-			if host == allowedHost {
-				return nil
-			}
-			return fmt.Errorf("acme/autocert: only %s host is allowed", allowedHost)
-		}
-		dataDir := "."
-		m = &autocert.Manager{
-			Prompt:     autocert.AcceptTOS,
-			HostPolicy: hostPolicy,
-			Cache:      autocert.DirCache(dataDir),
-		}
-
-		srv := &http.Server{
-			Addr:         ":443",
-			ReadTimeout:  5 * time.Second,
-			WriteTimeout: 10 * time.Second,
-			TLSConfig:    &tls.Config{GetCertificate: m.GetCertificate},
-		}
-		fmt.Println("ListenAndServe HTTPS 443")
-		srv.ListenAndServe()
-
-	}()
+	//go func() {
+	//	var m *autocert.Manager
+	//	hostPolicy := func(ctx context.Context, host string) error {
+	//		// Note: change to your real host
+	//		allowedHost := "sandbox.bolt-play.com"
+	//		if host == allowedHost {
+	//			return nil
+	//		}
+	//		return fmt.Errorf("acme/autocert: only %s host is allowed", allowedHost)
+	//	}
+	//	dataDir := "."
+	//	m = &autocert.Manager{
+	//		Prompt:     autocert.AcceptTOS,
+	//		HostPolicy: hostPolicy,
+	//		Cache:      autocert.DirCache(dataDir),
+	//	}
+	//
+	//	srv := &http.Server{
+	//		Addr:         ":443",
+	//		ReadTimeout:  5 * time.Second,
+	//		WriteTimeout: 10 * time.Second,
+	//		TLSConfig:    &tls.Config{GetCertificate: m.GetCertificate},
+	//	}
+	//	fmt.Println("ListenAndServe HTTPS 443")
+	//	srv.ListenAndServe()
+	//
+	//}()
 
 	srv := &http.Server{
 		Addr:         ":" + Router.Port,
